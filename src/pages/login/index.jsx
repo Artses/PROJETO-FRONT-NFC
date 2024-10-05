81d12956ec58c4f488204f86b8fecf89cb6a5267
Matheus Couto <coutom580@gmail.com>
2024-10-04 21:42:00 -0300

    inicio da estilização

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,23 @@
 
 import './style.css'
-
+import Pc from '../../assets/pc_img_login.png'
 function Login() {
 
   return (
     
-      <div>
-        <h1>olá mundo</h1>
+      <div className='container'>
+        <div>
+          <h1>Bem-vindo</h1>
+          <p>Faça seu login</p>
+          <img src={Pc} alt="" />
+        </div>
+        <form action="">
+          <p>Ainda não tem login?<a href="">Cadastro</a></p>
+          <h1>Login</h1>
+          <input type="text" name='name'/>
+          <input type="password" name='password'/>
+          <button type='button'>Cadastrar</button>
+        </form>
       </div>
       
   )
