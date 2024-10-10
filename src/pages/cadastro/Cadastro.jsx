318e6022e664f7a52d3a9cc715c67f1e02d3b0cc
Matheus Couto <coutom580@gmail.com>
2024-10-10 20:34:47 -0300

    cadastro_sendo_construido

--- src/pages/cadastro/Cadastro.jsx
+++ src/pages/cadastro/Cadastro.jsx
@@ -1,8 +1,31 @@
-import './style.css'
-
+import './cadastro.css'
+import { Link } from "react-router-dom"
 const Cadastro = () =>{
     return(
-        <h1>Pagina cadastro</h1>
+      <div className='container'>
+        <div className='div_bemvindo'>
+          <h1>BEM-VINDO</h1>
+          <p>Faça seu cadastro para utilizar o sistema</p>
+        </div>
+        <form action="" >
+          <h1>Cadastro</h1>
+          <p>Já tem conta? <Link to="/index">Entre aqui</Link></p>
+
+          <div>
+            <input type="text" name='name' placeholder='Nome' />
+            <input type="email" name='email' placeholder='E-mail' />
+          </div>
+
+          <div>
+            <input type="text" name='cep' placeholder='CEP'/>
+            <input type="text" name='cnpj' placeholder='CNPJ' />
+            <input type="password" name='password' placeholder='Senha' />
+            <input type="password" name='password' placeholder='Confirmar senha' />
+          </div>
+          <button type='button' className='button-37'>Cadastrar</button>
+        </form>
+      </div>
+
     )
 }
 export default Cadastro
