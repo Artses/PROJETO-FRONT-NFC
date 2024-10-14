5f889ee0f55a6681c1bc001f268a894f9da1008c
Matheus Couto <coutom580@gmail.com>
2024-10-13 23:08:54 -0300

    começando view tabela react table

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/login'
 import Cadastro from './pages/cadastro/Cadastro'
-import Home from './pages/home/home'
+import Home from './pages/home/Home'
+import View from './pages/view/View'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -13,11 +14,15 @@ const router = createBrowserRouter([
   },
   {
     path: '/Cadastro',
-    element: <Cadastro />
+    element: <Cadastro />,
   },
   {
     path:'/Home',
-    element: <Home/>
+    element: <Home/>,
+  },
+  {
+    path:'/View',
+    element:<View/>,
   }
 ]);
 
--- src/pages/view/View.jsx
+++ src/pages/view/View.jsx
@@ -1,9 +1,9 @@
-import './home.css'
-import Usuario from '../../assets/do-utilizador.png'
-const Home =()=>{
+import './view.css'
+
+const View =()=>{
     return(
         <div>
-        <div className='navbar'>
+            <div className='navbar'>
             
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path  d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
 
@@ -13,25 +13,10 @@ const Home =()=>{
 
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path  d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>   
         </div>
-        <div className='container_home'>
-            <div className='perfil'>
-                <img src={Usuario} alt="" />
-                <div>
-                    <h1>NOME DO USUARIO</h1>
-                    <p>Plano: Personalizado</p>
-                </div>
-                <hr className='linha-horizontal'></hr>
-            </div>
-            <div className='descricao'>
-                <h1>Banco Funcional de Presença</h1>
-                <p>Nosso aplicativo foi criado para simplificar o registro de presença em diversos ambientes, como escolas, empresas e outros locais onde é necessário monitorar a presença de pessoas. Usando tecnologias automatizadas, como NFC, biometria e outras, o sistema permite o registro de forma rápida e segura. Todas as informações são armazenadas em um banco de dados central, oferecendo um monitoramento eficiente e em tempo real. Facilite o controle de presença com uma solução moderna e versátil!</p>
-            </div>
+        <div className='container_view'>
+            <h1>Visualizar e editar</h1>
         </div>
-
         </div>
-
     )
 }
-
-
-export default Home
+export default View
